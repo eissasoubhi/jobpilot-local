8db4a37fb2befd52f8b3c529a5f51dcb3fde0d25
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-04 02:05:14 +0200

    Repair missing CV links when applications are loaded

--- api/src/Controller/ApplicationController.php
+++ api/src/Controller/ApplicationController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Application;
+use App\Service\ApplicationCvRepairService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,19 +11,26 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/api/applications')]
 final class ApplicationController
 {
-    public function __construct(private EntityManagerInterface $em) {}
+    public function __construct(
+        private EntityManagerInterface $em,
+        private ApplicationCvRepairService $cvRepair,
+    ) {}
 
     #[Route('', methods: ['GET'])]
     public function list(): JsonResponse
     {
-        $items = $this->em->getRepository(Application::class)->findBy([], ['updatedAt'=>'DESC']);
-        return new JsonResponse(array_map(static fn(Application $a) => $a->toArray(), $items));
+        $items = $this->em->getRepository(Application::class)->findBy([], ['updatedAt' => 'DESC']);
+        $this->cvRepair->repairAll($items);
+
+        return new JsonResponse(array_map(static fn (Application $application) => $application->toArray(), $items));
     }
 
     #[Route('/{id}', methods: ['PATCH'])]
     public function update(Application $application, Request $request): JsonResponse
     {
-        $application->fill($request->toArray()); $this->em->flush();
+        $application->fill($request->toArray());
+        $this->em->flush();
+
         return new JsonResponse($application->toArray());
     }
 }

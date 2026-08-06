b465a33911efec9d943290e50261dab012aa828b
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-06 02:06:38 +0200

    Link search criteria from main navigation

--- web/components/Shell.tsx
+++ web/components/Shell.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 
 const links = [
   ['/', 'Tableau de bord', '⌂'], ['/offres', 'Offres', '◎'], ['/connecteurs', 'Connecteurs', '⛓'],
+  ['/criteres-recherche', 'Critères de recherche', '⌕'],
   ['/candidatures', 'Candidatures', '✓'], ['/crm', 'CRM', '◇'], ['/positionnements', 'Positionnements', '⇄'],
   ['/messages', 'Messagerie', '✉'], ['/cv', 'Mes CV', '▤'], ['/profil', 'Profil', '◉'],
   ['/parametres', 'Paramètres', '⚙'],

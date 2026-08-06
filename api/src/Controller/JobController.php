a33e00c8e73f402eca439961c1af9dd1e9f409c1
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-07 00:38:28 +0200

    Keep preparation separate from submission

--- api/src/Controller/JobController.php
+++ api/src/Controller/JobController.php
@@ -7,7 +7,6 @@ namespace App\Controller;
 use App\Entity\JobOffer;
 use App\JobCatalog\Application\CanonicalJobOfferService;
 use App\Service\ApplicationPreparationService;
-use App\Service\AutomaticSubmissionService;
 use App\Service\JobProcessor;
 use App\Service\LocalDataService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -24,7 +23,6 @@ final class JobController
         private JobProcessor $processor,
         private CanonicalJobOfferService $canonicalJobs,
         private ApplicationPreparationService $preparation,
-        private AutomaticSubmissionService $automaticSubmission,
     ) {
     }
 
@@ -78,9 +76,7 @@ final class JobController
     #[Route('/{id}/prepare', methods: ['POST'])]
     public function prepare(JobOffer $job): JsonResponse
     {
-        $settings = $this->data->settings();
         $application = $this->preparation->prepare($job, $this->data->profile());
-        $this->automaticSubmission->submitIfEligible($application, $settings);
 
         return new JsonResponse($application->toArray());
     }

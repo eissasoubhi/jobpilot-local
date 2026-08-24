9a6b7c166c12b5ae47718757eb72503bffe1b6e8
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-24 18:42:39 +0200

    ranking: add matching score version store

---
+++ api/src/Service/MatchingScoreVersionStore.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\JobOffer;
+use App\Entity\JobOfferMatchingScoreState;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class MatchingScoreVersionStore
+{
+    public function __construct(private readonly EntityManagerInterface $em)
+    {
+    }
+
+    public function versionFor(JobOffer $job): int
+    {
+        $state = $this->em->getRepository(JobOfferMatchingScoreState::class)->findOneBy(['jobOffer' => $job]);
+
+        return $state instanceof JobOfferMatchingScoreState ? $state->getVersion() : 0;
+    }
+
+    public function mark(JobOffer $job, int $version = MatchingScoreVersion::CURRENT): void
+    {
+        $repository = $this->em->getRepository(JobOfferMatchingScoreState::class);
+        $state = $repository->findOneBy(['jobOffer' => $job]);
+
+        if (!$state instanceof JobOfferMatchingScoreState) {
+            $state = new JobOfferMatchingScoreState($job, $version);
+            $this->em->persist($state);
+
+            return;
+        }
+
+        $state->markVersion($version);
+    }
+}

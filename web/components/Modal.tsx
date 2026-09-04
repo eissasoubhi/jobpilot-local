950fde7647e0e261dde299df885c479527a3ee2d
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-09-04 21:52:57 +0200

    UX: skip hidden inputs in modal focus order

--- web/components/Modal.tsx
+++ web/components/Modal.tsx
@@ -5,7 +5,7 @@ import { KeyboardEvent, useEffect, useRef, type RefObject } from 'react';
 const FOCUSABLE_SELECTOR = [
   'a[href]',
   'button:not([disabled])',
-  'input:not([disabled])',
+  'input:not([type="hidden"]):not([disabled])',
   'select:not([disabled])',
   'textarea:not([disabled])',
   '[tabindex]:not([tabindex="-1"])',
